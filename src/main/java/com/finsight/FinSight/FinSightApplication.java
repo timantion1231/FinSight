a16702baeb162474271205a46a6fb4cd0fe79476
Timur Khanafin <mindiisky@ya.ru>
2025-04-27 15:39:56 +0300

    first commit

---
+++ src/main/java/com/finsight/FinSight/FinSightApplication.java
@@ -0,0 +1,13 @@
+package com.finsight.FinSight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinSightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinSightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finsight/FinSight/FinSightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finsight.FinSight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinSightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
